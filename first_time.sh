bda2185634a3a36d517859cb1b3832553bcfd719
ericleman <eric.leman@gmail.com>
2022-06-15 15:17:32 +0200

    Update first_time.sh

--- first_time.sh
+++ first_time.sh
@@ -1,1 +1,6 @@
+cd /home/eric && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+yay -S pamac-aur
+yay -S google-chrome
+yay -S gnome-shell-extension-dash-to-dock
+
 chown -R eric:eric /personal
