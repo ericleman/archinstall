d4f09b8c96479312a0f2aba4419fd356ba27c36e
ericleman <eric.leman@gmail.com>
2022-06-15 15:50:29 +0200

    Update first_time.sh

--- first_time.sh
+++ first_time.sh
@@ -2,6 +2,8 @@ cd /home/eric && git clone https://aur.archlinux.org/yay.git && cd yay && makepk
 yay -S pamac-aur
 yay -S google-chrome
 yay -S gnome-shell-extension-dash-to-dock
+sudo pacman -S --noconfirm --needed dkms
+yay -S rtl88xxau-aircrack-dkms-git
 
 sudo chown -R eric:eric /archinstall/personal
 cp -rv /archinstall/personal/. /home/eric
