5cb0c4bee455e5350129ce39facdcc53939b8131
ericleman <eric.leman@gmail.com>
2022-06-30 21:12:00 +0200

    Delete ansible directory

--- ansible/personal/conky/Alfirk/Alfirk/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-killall conky
-sleep 2s
-
-conky -c $HOME/.config/conky/Alfirk/Alfirk.conf &> /dev/null &
-#conky -c $HOME/.config/conky/Alfirk/Alfirk2.conf &> /dev/null &
-#conky -c $HOME/.config/conky/Alfirk/Alfirk3.conf &> /dev/null &
