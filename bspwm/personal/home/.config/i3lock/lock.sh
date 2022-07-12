3b174c9e5e5d7e7601437433de3dbffd83743bee
ericleman <eric.leman@gmail.com>
2022-07-12 17:56:32 +0200

    update 050-core-apps.yaml, main.yaml, powermenu and powermenu.rasi

---
+++ bspwm/personal/home/.config/i3lock/lock.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+BLANK='#00000000'
+CLEAR='#A3BE8C'
+DEFAULT='#88C0D0'
+TEXT='#88C0D0'
+WRONG='#BF616A'
+VERIFYING='#EBCB8B'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+--screen 1                   \
+--blur 9                     \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M:%S"        \
+--date-str="%A, %Y-%m-%d"       \
+--keylayout 1                \
