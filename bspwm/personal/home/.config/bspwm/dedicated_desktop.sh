3468726a76eeabd1c5eaf67f9d1eaf1757aadbca
ericleman <eric.leman@gmail.com>
2022-07-20 16:39:45 +0200

    update 020-core-apps.yaml, bspwmrc and sxhkdrc

---
+++ bspwm/personal/home/.config/bspwm/dedicated_desktop.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+focused_window=$(bspc query -N -n focused.window)
+json_fw=$(bspc query -T -n $focused_window)
+class_fw=$(echo $json_fw | jq -r '.client.className')
+
+case $class_fw in
+    'kitty')
+		desktop_icon=''
+        ;;
+    'Google-chrome')
+		desktop_icon=''
+        ;;
+    'Thunar')
+		desktop_icon=''
+        ;;
+    'Gedit')
+		desktop_icon=''
+        ;;
+    *)
+		desktop_icon='缾'
+        ;;
+esac
+
+number_desktop=$(bspc query -D | wc -l)
+
+new_desktop="$(( $number_desktop + 1 ))-$desktop_icon"
+
+bspc monitor -a "$new_desktop"
+
+bspc node -d "$new_desktop" --follow
