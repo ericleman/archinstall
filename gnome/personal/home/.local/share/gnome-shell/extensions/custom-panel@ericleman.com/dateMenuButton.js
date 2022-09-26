de3362a019af648457d1a54bc84368ba0278ce57
ericleman <eric.leman@gmail.com>
2022-09-26 17:05:08 +0200

    update 10 files and delete 1 file

--- gnome/personal/home/.local/share/gnome-shell/extensions/custom-panel@ericleman.com/dateMenuButton.js
+++ gnome/personal/home/.local/share/gnome-shell/extensions/custom-panel@ericleman.com/dateMenuButton.js
@@ -2,11 +2,11 @@ const DateMenu = imports.ui.main.panel.statusArea.dateMenu;
 
 
 var enable = () => {
-  DateMenu.add_style_class_name('custom-color4');
+  DateMenu.add_style_class_name('custom-color5');
 
 }
 
 var disable = () => {
-  DateMenu.remove_style_class_name('custom-color4');
+  DateMenu.remove_style_class_name('custom-color5');
 }
 
